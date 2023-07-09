e23163ac09e2eb0e8ba59a4a6f0bda0bbf227c67
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-09 09:55:23 +0330

    setting up router and start creating the choose sity modal of the website

--- bazargam-webstore/src/App.vue
+++ bazargam-webstore/src/App.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
+  import ChooseCity from './components/ChooseCity.vue'
 </script>
 
 <template>
   
+
+  <teleport to=".chooseCityModal">
+    <ChooseCity>  
+      <h1> a simple text </h1>
+      <template v-slot:links>
+        <a href="#"> Sign up </a> 
+        <a href="#"> more info </a>
+      </template>
+    </ChooseCity>    
+  </teleport>
 </template>
 
 <style scoped>
---
+++ bazargam-webstore/src/components/ChooseCity.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="chooseCityModal">
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// const nameFunc = (var: inputType): returnType => {...}
+
+const closeModal = () => {
+    
+}
+
+
+</script>
+
+<style>
+
+</style>
