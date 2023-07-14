b8c5a836d8b6fa66399b2719bc3a3ae10819c51e
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-14 13:08:38 +0330

    fixing a bug in reponsivibility of the modal

--- bazargam-webstore/src/components/ChooseCity.vue
+++ bazargam-webstore/src/components/ChooseCity.vue
@@ -1,60 +1,63 @@
 <template>
     <div v-if="isVisible" class="modal">
-        <div class="modal-bg" @click="bgClick"></div>
-        <div class="modal-container">
-            <img class="backdrop-img" src="../images/IRAN_SKYLINE.png" alt="iran_skyline">
-
-            <div class="modal-title">
-                <slot name="title"></slot>
-            </div>
-
-            <div class="modal-content">
-                <slot name="content"></slot>
-            </div>
+      <div class="modal-bg" @click="bgClick"></div>
+      <div class="modal-container">
+        <img
+          class="backdrop-img"
+          src="../images/IRAN_SKYLINE.png"
+          alt="iran_skyline"
+        />
+  
+        <div class="modal-title">
+          <slot name="title"></slot>
         </div>
+  
+        <div class="modal-content">
+          <slot name="content"></slot>
+        </div>
+      </div>
     </div>
-</template>
-
-<script setup lang="ts">
-withDefaults(defineProps<{isVisible: boolean}>(), {
-  isVisible: false
-})
-
-const emit = defineEmits<{
-    (e: "update:isVisible", value: boolean): void,
-}>();
-
-const bgClick = () => {
-    console.log("[Modal] bg click")
-    emit('update:isVisible', false);
-}
-
-</script>
-
-<style scoped>
-.modal {
+  </template>
+  
+  <script setup lang="ts">
+  withDefaults(defineProps<{ isVisible: boolean }>(), {
+    isVisible: false,
+  });
+  
+  const emit = defineEmits<{
+    (e: "update:isVisible", value: boolean): void;
+  }>();
+  
+  const bgClick = () => {
+    console.log("[Modal] bg click");
+    emit("update:isVisible", false);
+  };
+  </script>
+  
+  <style scoped>
+  .modal {
     position: fixed;
     top: 0;
     left: 0;
     width: 100vw;
-    height: 100vh;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-}
-
-.modal-bg {
+  }
+  
+  .modal-bg {
     background-color: rgba(0, 0, 0, 0.4);
     width: 100%;
     height: 100%;
     position: absolute;
-    top:0;
+    top: 0;
     left: 0;
     z-index: 1;
-}
-
-.modal-container {
+  }
+  
+  .modal-container {
     z-index: 10;
     display: flex;
     flex-direction: column;
@@ -65,29 +68,33 @@ const bgClick = () => {
     border-radius: 10px;
     min-height: 400px;
     max-height: 720px;
-    position: relative;
-}
-
-.modal-title {
+    position: fixed;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  
+  .modal-title {
     text-align: center;
     color: #4f4f4f;
     display: flex;
     justify-content: space-between;
     align-items: center;
-}
-
-.backdrop-img {
+  }
+  
+  .backdrop-img {
     position: absolute;
     bottom: 0;
     width: 100%;
     box-sizing: border-box;
     border-radius: inherit;
     z-index: -1;
-}
-
-.modal-content {
+  }
+  
+  .modal-content {
     padding: 0 30px;
     margin: 0;
     overflow: scroll;
-}
-</style>
+  }
+  </style>
+  
---
+++ bazargam-webstore/src/constants.ts
@@ -0,0 +1,233 @@
+export const cities = [
+    {
+        id: 1,
+        title: "تهران",
+        name: "tehran",
+        link: "#/tehran"
+    },
+
+    {
+        id: 2,
+        title: "یزد",
+        name: "yazd",
+        link: "#/yazd"
+    },
+
+    {
+        id: 3,
+        title: "اصفهان",
+        name: "isfahan",
+        link: "#/isfahan"
+    },
+
+    {
+        id: 4,
+        title: "کرمان",
+        name: "kerman",
+        link: "#/kerman"
+    },
+
+    {
+        id: 5,
+        title: "کرج",
+        name: "karaj",
+        link: "#/karaj"
+    },
+
+    {
+        id: 6,
+        title: "مشهد",
+        name: "mashhad",
+        link: "#/mashhad"
+    },
+
+    {
+        id: 7,
+        title: "شیراز",
+        name: "shiraz",
+        link: "#/shiraz"
+    },
+
+    {
+        id: 8,
+        title: "ارومیه",
+        name: "urumiyeh",
+        link: "#/urumiyeh"
+    },
+
+    {
+        id: 9,
+        title: "اهواز",
+        name: "ahvaz",
+        link: "#/ahvaz"
+    },
+
+    {
+        id: 10,
+        title: "کرمانشاه",
+        name: "kermanshah",
+        link: "#/kermanshah"
+    },
+
+    {
+        id: 11,
+        title: "تبریز",
+        name: "tabriz",
+        link: "#/tabriz"
+    },
+
+    {
+        id: 12,
+        title: "سنندج",
+        name: "sanandaj",
+        link: "#/sanandaj"
+    },
+
+    {
+        id: 13,
+        title: "گرگان",
+        name: "gorgan",
+        link: "#/gorgan"
+    },
+
+    {
+        id: 14,
+        title: "قم",
+        name: "qom",
+        link: "#/qom"
+    },
+
+    {
+        id: 15,
+        title: "بندرعباس",
+        name: "bandarabbas",
+        link: "#/bandarabbas"
+    },
+
+    {
+        id: 16,
+        title: "رشت",
+        name: "rasht",
+        link: "#/rasht"
+    },
+
+    {
+        id: 17,
+        title: "زاهدان",
+        name: "zahedan",
+        link: "#/zahedan"
+    },
+
+    {
+        id: 18,
+        title: "سمنان",
+        name: "semnan",
+        link: "#/semnan"
+    },
+
+    {
+        id: 19,
+        title: "بجنورد",
+        name: "bojnourd",
+        link: "#/bojnourd"
+    },
+
+    {
+        id: 20,
+        title: "یاسوج",
+        name: "yasouj",
+        link: "#/yasouj"
+    },
+
+    {
+        id: 21,
+        title: "شهرکرد",
+        name: "shahrekord",
+        link: "#/shahrekord"
+    },
+
+    {
+        id: 22,
+        title: "کاشان",
+        name: "kashan",
+        link: "#/kashan"
+    },
+
+    {
+        id: 23,
+        title: "لواسان",
+        name: "lavasan",
+        link: "#/lavasan"
+    },
+
+    {
+        id: 24,
+        title: "ایلام",
+        name: "ilam",
+        link: "#/ilam"
+    },
+
+    {
+        id: 25,
+        title: "قزوین",
+        name: "qazvin",
+        link: "#/qazvin"
+    },
+
+    {
+        id: 26,
+        title: "اراک",
+        name: "arak",
+        link: "#/arak"
+    },
+
+    {
+        id: 27,
+        title: "اردبیل",
+        name: "ardabil",
+        link: "#/ardabil"
+    },
+
+    {
+        id: 28,
+        title: "همدان",
+        name: "hamedan",
+        link: "#/hamedan"
+    },
+
+    {
+        id: 29,
+        title: "ساری",
+        name: "sari",
+        link: "#/sari"
+    },
+
+    {
+        id: 30,
+        title: "زنجان",
+        name: "zanjan",
+        link: "#/zanjan"
+    },
+
+    {
+        id: 31,
+        title: "بوشهر",
+        name: "boushehr",
+        link: "#/boushehr"
+    },
+
+    {
+        id: 32,
+        title: "بیرجند",
+        name: "birjand",
+        link: "#/birjand"
+    },
+
+    {
+        id: 33,
+        title: "خرم آباد",
+        name: "khorramabad",
+        link: "#/khorramabad"
+    }
+]
+
