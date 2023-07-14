c458047e2e7b1ce5ab58ef2cc40f061512836b06
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-07-14 13:08:38 +0330

    fixing a bug in reponsivibility of the modal

--- bazargam-webstore/src/App.vue
+++ bazargam-webstore/src/App.vue
@@ -1,273 +1,47 @@
 <script setup lang="ts">
   import { ref } from 'vue';
+  import { RouterView, RouterLink } from "vue-router";
+
   import ChooseCity from './components/ChooseCity.vue'
+  
+  import { cities } from "./constants";
+
   const modalIsVisible = ref(true);  
+  const currentPath = ref(window.location.hash);
+
+  window.addEventListener("hashchange", () => {
+    currentPath.value = window.location.hash;
+  });
 </script>
 
 <template>
   <!-- this component is the context of the choose city modal (from S.A.F.) -->
-  <ChooseCity v-model:isVisible="modalIsVisible" >  
+  <ChooseCity v-model:isVisible="modalIsVisible">
     <template v-slot:title>
-    <div class="choosecity-title-container">
-     
-
+      <div class="choosecity-title-container">
         <div class="title-text">
-          <h3 id="modal-title-content-h3"> انتخاب شهر </h3>
-          <p> .برای مشاهده محصولات ابتدا شهر خود را مشخص کنید </p>
+          <h3 id="modal-title-content-h3">انتخاب شهر</h3>
+          <p>.برای مشاهده محصولات ابتدا شهر خود را مشخص کنید</p>
         </div>
-     
+
         <div>
-          <img src="./images/logo.png" alt="logo" class="title-logo">
+          <img src="./images/logo.png" alt="logo" class="title-logo" />
         </div>
       </div>
     </template>
 
     <template v-slot:content>
       <div class="cities-container">
-        <a href="#">
-        <div class="cityBox">
-          <h4> تهران </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> یزد </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> اصفهان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> کرمان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> کرج </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> مشهد </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> شیراز </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> ارومیه </h4>
-        </div>
-      </a>
-      
-      <a href="#">
-        <div class="cityBox">
-          <h4> اهواز </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> کرمانشاه </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> تبریز </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> سنندج </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> گرگان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> قم </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> بندرعباس </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> رشت </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> زاهدان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> سمنان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> بجنورد </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> یاسوج </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> شهرکرد </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> کاشان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> لواسان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> ایلام </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> قزوین </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> اراک </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> اردبیل </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> همدان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> ساری </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> زنجان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> بوشهر </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> بیرجند </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> خرم آباد </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> بهمئی </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> گچساران </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> دهدشت </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> چناران, گلبهار </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> طرقبه, شاندیز </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> فریمان </h4>
-        </div>
-      </a>
-
-      <a href="#">
-        <div class="cityBox">
-          <h4> ملک آباد, بینالود </h4>
-        </div>
-      </a>
-      </div>      
+        <RouterLink v-for="city in cities" :key="city.id" :to="city.link">
+          <div class="cityBox">
+            <h4>{{ city.title }}</h4>
+          </div>
+        </RouterLink>
+      </div>
     </template>
-  </ChooseCity>    
-  
+  </ChooseCity>
 
+  <RouterView />
 </template>
 
 <style scoped>
