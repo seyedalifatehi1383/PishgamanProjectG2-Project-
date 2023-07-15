4820f0dac865acd0daecf07013e86ec8f14abca9
elmiraabolfazli <efabolfazli@gmail.com>
2023-07-15 22:06:57 +0330

    json-server

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,34 @@
 <template>
-  
+  <div  v-for="product in products" :key="product.id " class="products">
+    <div class="product"></div>
+    <router-link to=""><h1>{{product.title}}</h1></router-link>
+    <h3>{{ product.price }}</h3>
+    <h3>{{ product.details }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      products:[]
+    }
+  },
 mounted(){
-  fetch()
+  fetch('http://localhost:3000/products')
+   .then(res =>res.json())
+   .then(data =>this.products=data)
+   .catch(err=>console.log(err))
 }
 }
 </script>
 
 <style>
+.products{
+  background-color: bisque;
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+}
 
 </style>
